83d0b936cf8500ec40dae1ef998023180d47548b
marcoody <57504229+marcoody@users.noreply.github.com>
2020-05-11 17:09:31 +0200

    Update data.h

--- data.h
+++ data.h
@@ -13,14 +13,23 @@ public:
     data(u_int g, u_int m, u_int a);
     data(const data& d);
     data& operator=(const data& d);
-    ~data(){}
+    ~data(){};
 
     u_int getGiorno() const;
     u_int getMese()const;
     u_int getAnno()const;
+    bool isBisestile()const;
+    //è meglio che ritorni un'altra data al posto di modif l'ogg di invocazione? (e renderla const)
+    void avanzaUnGiorno();
+
+    data& operator++();
+    data operator++(int);
+    data operator+(int) const;
+    bool operator==(data) const;
 
 };
 
+std::ostream& operator<<(std::ostream&, data);
 //la metto fuori perchè non voglio che abbia un ogg di invocazione
 //oppure è meglio mettela dentro, creare un ogg temporaneo con i dati di costruzione e poi eliminarlo?
 bool checkData(u_int g, u_int m, u_int a);
