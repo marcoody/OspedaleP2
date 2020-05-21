1ef7169374510ff1d320366c6a8714882e335f5d
marcoody <57504229+marcoody@users.noreply.github.com>
2020-05-21 19:09:18 +0200

    Add files via upload

--- persona.cpp
+++ persona.cpp
@@ -1,27 +1,77 @@
 #include "persona.h"
+//PERSONA:
+//metodi di get
+unsigned short Persona::getIdPersona(){ return idPersona;}
+std::string Persona::getNomePersona(){return nomePersona;}
+std::string Persona::getCognPersona(){return cognPersona;}
+data Persona::getDataNascita(){ return dataNascita;}
+gender Persona::getGenere(){ return genere;}
 
-// Costruttore di default
-Persona::Persona() : Id("#000000"), Nome("Mario"), Cognome("Rossi") {}
+//metodi per cambiare i campi modificabili
+void Persona::cambiaNome(std::string& s){nomePersona=s;}
+void Persona::cambiaCogn(std::string& s){cognPersona=s;}
+void Persona::cambiaDataNascita(const data& d){dataNascita=d;}
+void Persona::cambiaGenere(gender g){genere=g;}
 
-// Costruttore
-Persona::Persona(string i, string n, string c, const Data& d) :
-  Id(i), Nome(n), Cognome(c), DataNascita(d) {}
-
-// Costruttore di copia
-Persona::Persona(const Persona& p) {
-  Id = p.Id;
-  Nome = p.Nome;
-  Cognome = p.Cognome;
-  DataNascita = p.DataNascita;
+Persona::Persona(unsigned short id, std::string n, std::string c, const data & d, const gender & g): idPersona(id), nomePersona(n), cognPersona(c), dataNascita(d), genere(g){}
+Persona::Persona(const Persona & p){
+    idPersona=p.idPersona;
+    nomePersona=p.nomePersona;
+    cognPersona=p.cognPersona;
+    dataNascita=p.dataNascita;
+    genere=p.genere;
 }
 
-// Operatore di assegnazione
-Persona& Persona::operator=(const Persona & p) {
-  if(this != &p) {
-  	Id = p.Id;
-    Nome = p.Nome;
-    Cognome = p.Cognome;
-    DataNascita = p.DataNascita;
-  }
-  return *this;
-} 
+//DIPENDENTE:
+//metodi di get
+bool Dipendente::inTurno(const giorni &g) const{ return giorniTurno[g]; }
+unsigned short Dipendente::numGiorniLavoro() const{
+    u_int tot=0;
+    for(u_int i=0; i<7; ++i){tot+=giorniTurno[i];}
+    return tot;
+}
+void Dipendente::prendiPermesso(const giorni &g){ if(inTurno(g))giorniTurno[g]=0;}
+
+Dipendente::Dipendente(unsigned short id, std::string n, std::string c, const data & d, const gender & g, bool *t):Persona(id, n,c,d,g){
+    for(u_int i=0; i<7; i++){
+        giorniTurno[i]=t[i];
+    }
+}
+
+Dipendente::Dipendente(const Dipendente& d){
+    Persona::Persona(d);
+    for(u_int i=0; i<7; ++i){giorniTurno[i]=d.giorniTurno[i];}
+
+}
+
+//MEDICO:
+
+Medico::Medico(unsigned short id, std::string n, std::string c, const data & d, const gender & g, bool * t, std::vector<const Paziente *> p):Dipendente(id,n,c,d,g,t), pazienti(p){}
+
+
+double Medico::stipendio() const{ return pagaPerOraMedico*numGiorniLavoro();}
+
+std::vector<const Paziente *>& Medico::getPazienti(){ return pazienti;}
+
+void Medico::aggiungiPaziente(const Paziente *p){
+    pazienti.push_back(p);
+}
+void Medico::togliPaziente(const Paziente *p){
+    for(auto it=pazienti.begin(); it!=pazienti.end(); ++it){ if(*it==p){pazienti.erase(it);}}
+}
+
+
+//INFERMIERE:
+Infermiere::Infermiere(unsigned short id, std::string n, std::string c, const data & d, const gender & g, bool * t, bool r):Dipendente(id,n,c,d,g,t),responsabile(r) {}
+
+double Infermiere::stipendio() const{ return pagaPerOraInf*numGiorniLavoro();}
+bool Infermiere::isResponsabile() const{ return responsabile;}
+void Infermiere::cambiaResponsabile(bool x){responsabile=x;}
+
+//CHIRURGIA:
+
+
+
+//CHIRURGIA:
+Paziente::Paziente(unsigned short id, std::string n, std::string c, const Medico * m, const data & dn, const gender & g, const data & ir, const data & fr, bool dec): Persona(id,n,c,dn,g), medicoAssegnato(m), inizioRicovero(ir),fineRicovero(fr), deceduto(dec) {};
+
--- persona.h
+++ persona.h
@@ -1,22 +1,118 @@
 #ifndef PERSONA_H
 #define PERSONA_H
+#include <string>
+#include <iostream>
+#include <istream>
+#include <fstream>
 #include "data.h"
+#include <vector>
+enum giorni{lunedi=0, martedi=1, mercoledi=2, giovedi=3, venerdi=4, sabato=5, domenica=6};
+enum gender{maschio=0, femmina=1, altro=3};
+#define u_int unsigned short
 
-using std::string;
 
-class Persona {
-  private:
-  	string Id;
-  	string Nome;
-  	string Cognome;
-  	Data DataNascita;
-  public:
-  	Persona();
-  	Persona(string, string, string, const Data&);
-  	Persona(const Persona&);
-  	~Persona();
-  	Persona& operator=(const Persona&);
-	
+class Persona{
+private:
+    int idPersona;
+    std::string nomePersona;
+    std::string cognPersona;
+    data dataNascita;
+    gender genere;
+public:
+    virtual ~Persona(){};
+    virtual Persona* clone() const = 0;
+
+    //metodi get per i campi privati (da valutare se mettere protected)
+    u_int getIdPersona();
+    std::string getNomePersona();
+    std::string getCognPersona();
+    data getDataNascita();
+    gender getGenere();
+
+    //metodi per cambiare i campi privati (tutti tranne idPersona)
+    void cambiaNome(std::string&);
+    void cambiaCogn(std::string&);
+    void cambiaDataNascita(const data&);
+    void cambiaGenere(gender);
+
+
+protected:
+    Persona(u_int, std::string, std::string, const data& = data(), const gender& =altro);
+    Persona(const Persona&);
+
 };
 
-#endif
+class Dipendente: public Persona{
+private:
+    bool giorniTurno[7];
+public:
+    virtual Dipendente* clone() const=0;
+    virtual double stipendio()const=0;
+
+    bool inTurno(const giorni& g) const; //controlla se il giono g il dipendente è di turno, ritorna bool
+    u_int numGiorniLavoro() const; //controlla il numero di giorni alla sett in cui il dipendente lavora
+    void prendiPermesso(const giorni& g); //mette il giorno g in false, non lavorerà quel giorno
+protected:
+    Dipendente(u_int, std::string, std::string, const data& = data(), const gender& =altro, bool* =0);
+    Dipendente(const Dipendente&); //??
+
+};
+
+class Paziente; //dich incompleta per poter implementare la classe Medico
+
+class Medico: virtual public Dipendente{
+private:
+    static double pagaPerOraMedico;
+    std::vector<const Paziente*> pazienti;
+public:
+    Medico(u_int, std::string, std::string, const data& = data(), const gender& =altro, bool* =0, std::vector<const Paziente*> = std::vector<const Paziente*>());
+    Medico(const Medico&);
+    Medico& operator=(const Medico&);
+
+    virtual ~Medico(); //SERVE RIDEFINIRLO?
+    virtual double stipendio() const;
+
+    std::vector<const Paziente*>& getPazienti();
+
+    void aggiungiPaziente(const Paziente*);
+    void togliPaziente(const Paziente*);
+};
+static double pagaPerOraMedico=25.0;
+
+class Infermiere: virtual public Dipendente{
+private:
+    bool responsabile;
+    static double pagaPerOraInf;
+public:
+    Infermiere(u_int, std::string, std::string, const data& = data(), const gender& =altro, bool* =0, bool=0);
+
+    virtual double stipendio() const;
+
+    bool isResponsabile() const;
+    void cambiaResponsabile(bool); //cambia lo status dell'infermiere da responsabile a "normale" e viceversa
+};
+static double pagaPerOraInf=20.0;
+
+class Chirurgia: public Medico, public Infermiere{
+private:
+    //qui è meglio un array o un vector?
+    std::vector< const Dipendente*> equipe;
+    std::vector<const Paziente*> daOperare;
+public:
+    Chirurgia(const Medico*, const Infermiere*); //??
+
+    void cambiaEquipe(const Medico* =0, const Infermiere* =0);
+    void addPazienteDaOperare(const Paziente*);
+    void delPazienteDaOperare(const Paziente*);
+};
+
+class Paziente: public Persona{
+private:
+    data inizioRicovero;
+    data fineRicovero;
+    bool deceduto;
+    Medico* medicoAssegnato;
+public:
+    Paziente(u_int, std::string, std::string, const Medico*, const data& =data(), const gender& =altro, const data& =data(), const data& =data(), bool=0);
+};
+#endif // PERSONA_H
