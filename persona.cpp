c24a6fb42ee3f95b49af4470d9509f656fbe3285
Elia <45065106+WildPacificDark@users.noreply.github.com>
2020-05-21 19:39:03 +0200

    Update persona.cpp

--- persona.cpp
+++ persona.cpp
@@ -2,18 +2,18 @@
 //PERSONA:
 //metodi di get
 unsigned short Persona::getIdPersona(){ return idPersona;}
-std::string Persona::getNomePersona(){return nomePersona;}
-std::string Persona::getCognPersona(){return cognPersona;}
+string Persona::getNomePersona(){return nomePersona;}
+string Persona::getCognPersona(){return cognPersona;}
 data Persona::getDataNascita(){ return dataNascita;}
 gender Persona::getGenere(){ return genere;}
 
 //metodi per cambiare i campi modificabili
-void Persona::cambiaNome(std::string& s){nomePersona=s;}
-void Persona::cambiaCogn(std::string& s){cognPersona=s;}
-void Persona::cambiaDataNascita(const data& d){dataNascita=d;}
-void Persona::cambiaGenere(gender g){genere=g;}
+void Persona::setNome(string& s){nomePersona=s;}
+void Persona::setCogn(string& s){cognPersona=s;}
+void Persona::setdataNascita(const data& d){dataNascita=d;}
+void Persona::setGenere(gender g){genere=g;}
 
-Persona::Persona(unsigned short id, std::string n, std::string c, const data & d, const gender & g): idPersona(id), nomePersona(n), cognPersona(c), dataNascita(d), genere(g){}
+Persona::Persona(unsigned short id, string n, string c, const data & d, const gender & g): idPersona(id), nomePersona(n), cognPersona(c), dataNascita(d), genere(g){}
 Persona::Persona(const Persona & p){
     idPersona=p.idPersona;
     nomePersona=p.nomePersona;
@@ -32,26 +32,24 @@ unsigned short Dipendente::numGiorniLavoro() const{
 }
 void Dipendente::prendiPermesso(const giorni &g){ if(inTurno(g))giorniTurno[g]=0;}
 
-Dipendente::Dipendente(unsigned short id, std::string n, std::string c, const data & d, const gender & g, bool *t):Persona(id, n,c,d,g){
+Dipendente::Dipendente(unsigned short id, string n, string c, const data & d, const gender & g, bool *t):Persona(id, n,c,d,g){
     for(u_int i=0; i<7; i++){
         giorniTurno[i]=t[i];
     }
 }
 
-Dipendente::Dipendente(const Dipendente& d){
-    Persona::Persona(d);
+Dipendente::Dipendente(const Dipendente& d) : Persona(d) {
     for(u_int i=0; i<7; ++i){giorniTurno[i]=d.giorniTurno[i];}
-
 }
 
 //MEDICO:
 
-Medico::Medico(unsigned short id, std::string n, std::string c, const data & d, const gender & g, bool * t, std::vector<const Paziente *> p):Dipendente(id,n,c,d,g,t), pazienti(p){}
+Medico::Medico(unsigned short id, string n, string c, const data & d, const gender & g, bool * t, vector<const Paziente *> p):Dipendente(id,n,c,d,g,t), pazienti(p){}
 
 
 double Medico::stipendio() const{ return pagaPerOraMedico*numGiorniLavoro();}
 
-std::vector<const Paziente *>& Medico::getPazienti(){ return pazienti;}
+vector<const Paziente *>& Medico::getPazienti(){ return pazienti;}
 
 void Medico::aggiungiPaziente(const Paziente *p){
     pazienti.push_back(p);
@@ -62,7 +60,7 @@ void Medico::togliPaziente(const Paziente *p){
 
 
 //INFERMIERE:
-Infermiere::Infermiere(unsigned short id, std::string n, std::string c, const data & d, const gender & g, bool * t, bool r):Dipendente(id,n,c,d,g,t),responsabile(r) {}
+Infermiere::Infermiere(unsigned short id, string n, string c, const data & d, const gender & g, bool * t, bool r):Dipendente(id,n,c,d,g,t),responsabile(r) {}
 
 double Infermiere::stipendio() const{ return pagaPerOraInf*numGiorniLavoro();}
 bool Infermiere::isResponsabile() const{ return responsabile;}
@@ -73,5 +71,6 @@ void Infermiere::cambiaResponsabile(bool x){responsabile=x;}
 
 
 //CHIRURGIA:
-Paziente::Paziente(unsigned short id, std::string n, std::string c, const Medico * m, const data & dn, const gender & g, const data & ir, const data & fr, bool dec): Persona(id,n,c,dn,g), medicoAssegnato(m), inizioRicovero(ir),fineRicovero(fr), deceduto(dec) {};
+Paziente::Paziente(unsigned short id, string n, string c, const Medico * m, const data & dn, const gender & g, const data & ir, const data & fr, bool dec) :
+  Persona(id,n,c,dn,g), medicoAssegnato(m), inizioRicovero(ir),fineRicovero(fr), deceduto(dec) {};
 
