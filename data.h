c4d1a9ff1b650b8898d9d5f318e9db93a9954773
marcoody <57504229+marcoody@users.noreply.github.com>
2020-05-28 16:01:44 +0200

    Update data.h

--- data.h
+++ data.h
@@ -2,28 +2,26 @@
 #define DATA_H
 #include <iostream>
 
-//<Elia: Per convenienza, così ci si risparmia di dover scrivere ogni volta std::>
 using std::ostream;
 // Opzionali, se servono basta decommentare
 //using std::cout;
 //using std::endl;
 
-// <Elia: E' sempre meglio passare un riferimento costante, così ci risparmiamo
-// chiamate inutili al costruttore di copia, dopo se sbaglio correggetemi,
-// ma almeno rifacendomi al libro di P2, la maggior parte dei 'valori', sono 
-// passati così>
-
 #define u_int unsigned short
 enum giorni {lunedi=0, martedi=1, mercoledi=2, giovedi=3, venerdi=4, sabato=5, domenica=6};
+
 class Data {
     friend ostream& operator<<(ostream&, const Data&);
 private:
     u_int _giorno;
     u_int _mese;
     u_int _anno;
+
+    //servono per calcolare il giorno della settimana in base alla data//
+    int giorniDaInizioAnno() const;
+    int giorniDal1Gen1900() const;
 public:
-    //mettiamo un qualcosa di defalut?
-    Data(u_int = 1, u_int = 1, u_int = 1970);
+    Data(u_int = 1, u_int = 1, u_int = 1970); //DEFAULT?
     Data(const Data& d);
     Data& operator=(const Data& d);
     ~Data();
@@ -31,11 +29,8 @@ public:
     u_int getGiorno() const;
     u_int getMese() const;
     u_int getAnno() const;
-    //è meglio che ritorni un'altra data al posto di modif l'ogg di invocazione? (e renderla const)
-    // <Elia: Io direi che è meglio lasciarla cosi, perché se invoco avanzaUnGiorno(), mi
-    // aspetto che modifichi l'ogg d'invocazione avanzando di un giorno.>
     void avanzaUnGiorno();
-    
+
     // Operatore di incremento prefisso
     Data& operator++();
     // Operatore di incremento postfisso
@@ -44,16 +39,16 @@ public:
     Data operator+(int) const;
     // Operatore di uguaglianza
     bool operator==(const Data&) const;
-    //servono per calcolare il giorno della settimana in base alla data//
-    int giorniDaInizioAnno() const;
-    int giorniDal1Gen1900() const;
+    // Calcola il giorno della settimana in base alla data di invocazione
     giorni giornoSettimana()const;
-    
+
 };
 //li metto fuori perchè non mi serve l'ogg di invocazione
 bool isBisestile(u_int a);
 int giorniNelMese(u_int m, u_int a);
 //la metto fuori perchè non voglio che abbia un ogg di invocazione
-//oppure è meglio mettela dentro, creare un ogg temporaneo con i dati di costruzione e poi eliminarlo?
+int contaCifre(int a);
 bool checkData(u_int g, u_int m, u_int a);
+ostream& operator<<(ostream&, const Data&);
+ostream& operator<<(ostream&, const giorni&);
 #endif // DATA_H
