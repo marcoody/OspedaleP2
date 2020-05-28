c3bbb92a69b781e8dbe9c1ac4d03214f473f9ebc
marcoody <57504229+marcoody@users.noreply.github.com>
2020-05-28 16:03:08 +0200

    Update data.cpp

--- data.cpp
+++ data.cpp
@@ -1,28 +1,19 @@
 #include "data.h"
 
-bool checkData(u_int g, u_int m, u_int a){
-  if(m > 12 || g > 31) { return false; }
-  bool bisestile = isBisestile(a);
-  
-  if((m ==4 ||m == 6|| m == 9 || m == 11 ) && g > 30) { return false; }
-  if(m == 2 && g > 29) { return false; }
-  if(m == 2 && bisestile == false && g > 28) { return false; }
-  return true;
-}
-
+//costruttori
 Data::Data(u_int g, u_int m, u_int a){
   if(checkData(g,m,a)) {
-    _giorno = g; _mese = m; _anno = a;
+    _giorno = g; _mese = m;
+    if(contaCifre(a)==2){a+=1900;}
+    _anno=a;
   }
+  else{ throw std::runtime_error("Data non valida");}
+}
+Data::Data(const Data& d): _giorno(d._giorno), _mese(d._mese), _anno(d._anno) {
+  //qui non faccio il controllo della legittimità di d perchè è già stato fatto durante la creazione di data
 }
 
-Data::Data(const Data& d) {
-  //qui non faccio il controllo perchè è già stato fatto durante la creazione di data
-  _giorno = d._giorno;
-  _mese = d._mese;
-  _anno = d._anno;
-}
-
+//assegnazione e distruzione
 Data& Data::operator=(const Data& d){
   if(&d != this){
     _giorno = d._giorno;
@@ -31,31 +22,19 @@ Data& Data::operator=(const Data& d){
   }
   return *this;
 }
+Data::~Data(){}
 
-unsigned short Data::getGiorno() const { return _giorno; }
-unsigned short Data::getMese() const { return _mese; }
-unsigned short Data::getAnno() const { return _anno; }
+//metodi di get
+u_int Data::getGiorno() const { return _giorno; }
+u_int Data::getMese() const { return _mese; }
+u_int Data::getAnno() const { return _anno; }
 
-bool isBisestile(u_int a){
-  bool bisestile = false;
-  if(a % 4 == 0) {
-    bisestile = true;
-    if(a % 100 == 0) {
-      bisestile = false;
-      if(a % 400 == 0) { bisestile = true; }
-    }
-  }
-  return bisestile;
-}
-
+//manipolazione Data
 void Data::avanzaUnGiorno(){
     if((_mese == 4 || _mese == 6 || _mese == 9 || _mese == 11) && (_giorno == 30)) { ++_mese; _giorno = 1; }
     //qui non faccio controllo sul mese perchè se l'ogg data esiste, significa che è corretto(vedi costruttore)
     //quindi tutte le date corrette che hanno _giorno==31 avranno _mese==(mesi da 31 giorni)
-    else if(_giorno == 31) {
-      _giorno = 1;
-      ++_mese;
-    }
+    else if(_giorno == 31) {_giorno = 1; ++_mese;}
     else if(_mese == 2 && _giorno == 28 && !isBisestile(_anno)){ _giorno = 1; _mese = 3; }
     else if(_mese == 2 && _giorno == 28 && isBisestile(_anno)) { ++_giorno; }
     //il fatto se sia bisestile è un controllo in più che faccio per sicurezza
@@ -76,7 +55,7 @@ Data Data::operator++(int) {
   this->avanzaUnGiorno();
   return temp;
 }
- 
+
 Data Data::operator+(int x) const{
   Data tmp=*this;
   for(u_int i = 0; i < x; ++i) { tmp.avanzaUnGiorno(); }
@@ -84,17 +63,10 @@ Data Data::operator+(int x) const{
 }
 
 bool Data::operator==(const Data& d) const{
-  return (d.getGiorno() == _giorno) && (d.getMese() == _mese) 
-    && (d.getAnno() == _anno);
+  return (d.getGiorno() == _giorno) && (d.getMese() == _mese) && (d.getAnno() == _anno);
 }
 
-int giorniNelMese(u_int m, u_int a){
-    if(m==2 && isBisestile(a)){return 29;}
-    else if(m==2 && isBisestile(a)){return 28;}
-    else if(m==4 || m==6 || m==9 || m==11){return 30;}
-    else{return 31;}
-}
-
+//funzioni ausiliarie (LE METTO PRIVATE)
 int Data::giorniDaInizioAnno() const{
     int tot=0;
     for(u_int i=1; i<_mese; ++i){
@@ -105,14 +77,71 @@ int Data::giorniDaInizioAnno() const{
 }
 
 int Data::giorniDal1Gen1900() const{
-    int bis=((_anno-1900)/4);
+    int bis=((_anno-1900-1)/4);
     int tot= bis*(366)+giorniDaInizioAnno();
     if((_anno-1900) != 0){tot+=(_anno-1900-bis)*365;}
     return tot-1; //conto da zero
 }
 
+//PUBLIC
 giorni Data::giornoSettimana() const{ return giorni(giorniDal1Gen1900()%7);}
+
+
+//METODI ESTERNI
+bool isBisestile(u_int a){
+  bool bisestile = false;
+  if(a % 4 == 0) {
+    bisestile = true;
+    if(a % 100 == 0) {
+      bisestile = false;
+      if(a % 400 == 0) { bisestile = true; }
+    }
+  }
+  return bisestile;
+}
+int giorniNelMese(u_int m, u_int a){
+    if(m==2 && isBisestile(a)){return 29;}
+    else if(m==2 && !isBisestile(a)){return 28;}
+    else if(m==4 || m==6 || m==9 || m==11){return 30;}
+    else{return 31;}
+}
+//checkdata esterna alla classe perchè volevo assicurarmi che un oggetto data costruito fosse SEMPRE legittimo
+//-> se avessi messo la funz all'interno, avrebbe dovuro avere un ogg di invocazione di tipo data e non voglio che vengano creati data non legittimi
+int contaCifre(int a){
+    bool end=false; int cifre=1;
+    while(!end){
+        a/=10;
+        if(a!=0){++cifre;}
+        else{ end=true;}
+    }
+    return cifre;
+}
+bool checkData(u_int g, u_int m, u_int a){
+  if(m > 12 || g > 31) { return false; }
+  //controllo che gli anni siano scritti nella maniera corretta cioè AAAA oppure AA
+  int cifre=contaCifre(a);
+  if(cifre==3 || cifre==1 || cifre>4){return false;}
+  bool bisestile = isBisestile(a);
+
+  if((m ==4 ||m == 6|| m == 9 || m == 11 ) && g > 30) { return false; }
+  if(m == 2 && g > 29) { return false; }
+  if(m == 2 && bisestile == false && g > 28) { return false; }
+  return true;
+}
 
 ostream &operator<<(ostream &os, const Data& d){
   return os << d._giorno << "/" << d._mese << "/" << d._anno;
 }
+
+ostream &operator<<(ostream &os, const giorni &g){
+    if(g==0){ return os<<"lunedi";}
+    if(g==1){return os<<"martedi";}
+    if(g==2){return os<<"mercoledi";}
+    if(g==3){return os<<"giovedi";}
+    if(g==4){return os<<"venerdi";}
+    if(g==5){return os<<"sabato";}
+    if(g==6){return os<<"domenica";}
+}
+
+
+
