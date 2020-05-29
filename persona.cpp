4a0c9eef8ef43812bd3e7eaa7cba7a1d62bb9de7
marcoody <57504229+marcoody@users.noreply.github.com>
2020-05-29 03:18:10 +0200

    Update persona.cpp

--- persona.cpp
+++ persona.cpp
@@ -1,6 +1,25 @@
 #include "persona.h"
 //PERSONA:
 
+//operatori
+bool Persona::operator==(const Persona & p) const{ return idPersona==p.idPersona;}
+bool Persona::operator!=(const Persona & p) const{ return idPersona!=p.idPersona;}
+
+//metodi di get
+u_int Persona::getIdPersona(){ return idPersona;}
+string Persona::getNomePersona(){return nomePersona;}
+string Persona::getCognPersona(){return cognPersona;}
+Data Persona::getDataNascita(){ return dataNascita;}
+gender Persona::getGenere(){ return genere;}
+
+// Metodi di Set (NON SI PUò CAMBIARE ID_PERSONA)
+void Persona::cambiaNome(string& s){nomePersona=s;}
+void Persona::cambiaCogn(string& s){cognPersona=s;}
+void Persona::cambiaDataNascita(const Data& d){dataNascita=d;}
+void Persona::cambiaGenere(gender g){genere=g;}
+
+//PROTECTED: (li metto solo per poter creare costruttori e operatori vari nelle sottoclassi)
+
 /* Costruttore di Persona
 * param id: Identificativo
 * param n:  Nome
@@ -8,32 +27,40 @@
 * param d:  Data di Nascita
 * param g:  Genere
 */
-Persona::Persona(unsigned short id, string n, string c, const data& d, const gender& g) : 
-  idPersona(id), nomePersona(n), cognPersona(c), dataNascita(d), genere(g) {}
+Persona::Persona(u_int id, string n, string c, const Data& d, const gender& g): idPersona(id), nomePersona(n), cognPersona(c), dataNascita(d), genere(g){};
 
 // Costruttore di copia di Persona
-Persona::Persona(const Persona& p){
-    idPersona = p.idPersona;
-    nomePersona = p.nomePersona;
-    cognPersona = p.cognPersona;
-    dataNascita = p.dataNascita;
-    genere = p.genere;
+Persona::Persona(const Persona& p):idPersona(p.idPersona),nomePersona(p.nomePersona), cognPersona(p.cognPersona), dataNascita(p.dataNascita), genere(p.genere){};
+Persona& Persona::operator=(const Persona& p){
+   if(&p!= this){
+       idPersona=p.idPersona;
+       nomePersona=p.nomePersona;
+       cognPersona=p.cognPersona;
+       dataNascita=p.dataNascita;
+       genere=p.genere;
+   }
+   return *this;
 }
 
-// Metodi di Get
-unsigned short Persona::getIdPersona() const { return idPersona; }
-string Persona::getNomePersona() const { return nomePersona; }
-string Persona::getCognPersona() const { return cognPersona; }
-data Persona::getDataNascita() const { return dataNascita; }
-gender Persona::getGenere() const { return genere; }
-
-// Metodi di Set
-void Persona::cambiaNome(string& s) { nomePersona = s; }
-void Persona::cambiaCogn(string& s) { cognPersona = s; }
-void Persona::cambiaDataNascita(const data& d) { dataNascita = d; }
-void Persona::cambiaGenere(gender g) { genere = g; }
+/////////////////////////////////////////////////////////////////////////////////////
 
 //DIPENDENTE:
+
+//metodi di get
+bool Dipendente::inTurno(const giorni &g) const{ return giorniTurno.inturno(g);}
+u_int Dipendente::numGiorniLavoro() const{ return giorniTurno.giornilavoro(); }
+void Dipendente::cambiaTurno(const giorni& g1, const giorni& g2){ return giorniTurno.switchturno(g1,g2);}
+
+void Dipendente::aggiungiTurno(const giorni &g){
+    try{giorniTurno.addTurno(g);}
+    catch(Errori){ std::cerr<<"Errore: turno già esistente. Riprovare.";}
+}
+void Dipendente::eliminaTurno(const giorni &g){
+    try{giorniTurno.delTurno(g);}
+    catch(Errori){ std::cerr<<"Errore: turno già libero. Riprovare";}
+}
+
+//protected:
 
 /* Costruttore di Dipendente
 * param id: Identificativo
@@ -43,28 +70,25 @@ void Persona::cambiaGenere(gender g) { genere = g; }
 * param g:  Genere
 * param t:  Array booleano
 */
-Dipendente::Dipendente(unsigned short id, string n, string c, const data& d, const gender& g, bool* t) :Persona(id, n, c, d, g) {
-  for(u_int i = 0; i < 7; i++){
-    giorniTurno[i] = t[i];
-  }
-}
+Dipendente::Dipendente(u_int id, string n, string c, const Data & d, const gender & g, const Turno& t):Persona(id,n,c,d,g), giorniTurno(t){}
 
 // Costruttore di Copia di Dipendente
-Dipendente::Dipendente(const Dipendente& d) : Persona(d) {
-  for(u_int i = 0; i < 7; ++i) { giorniTurno[i] = d.giorniTurno[i]; }
+Dipendente::Dipendente(const Dipendente& d):Persona(d), giorniTurno(d.giorniTurno){}
+
+Dipendente &Dipendente::operator=(const Dipendente & d){
+    if(&d!=this){
+        this->Persona::operator=(d);
+        giorniTurno=d.giorniTurno;
+    }
+    return *this;
 }
 
-// Metodi di Get
-unsigned short Dipendente::numGiorniLavoro() const {
-    u_int tot = 0;
-    for(u_int i = 0; i < 7; ++i) { tot += giorniTurno[i]; }
-    return tot;
-}
-// param g: Riferimento costante ad enumerazione
-bool Dipendente::inTurno(const giorni& g) const { return giorniTurno[g]; }
-void Dipendente::prendiPermesso(const giorni& g) { if(inTurno(g)) giorniTurno[g] = 0; }
+
+/////////////////////////////////////////////////////////////////////////////////////
+
 
 //MEDICO:
+double Medico::pagaPerOraMedico=25.0;
 
 /* Costruttore di Medico
 * param id: Identificativo
@@ -73,30 +97,57 @@ void Dipendente::prendiPermesso(const giorni& g) { if(inTurno(g)) giorniTurno[g]
 * param d:  Data di Nascita
 * param g:  Genere
 * param t:  Array booleano
-* param p:  Vettore di puntatori ad oggetti di tipo Paziente 
+* param p:  Vettore di puntatori ad oggetti di tipo Paziente
 */
-Medico::Medico(unsigned short id, string n, string c, const data& d, const gender& g, bool * t, vector<const Paziente*> p) :
-  Dipendente(id, n, c, d, g, t), pazienti(p) {}
-
+Medico::Medico(u_int id, string n, string c, const Data & d, const gender & g, const Turno& t, vector<Paziente *> p):Dipendente(id,n,c,d,g,t), pazienti(p){}
+Medico::~Medico(){ pazienti.clear();}
+Medico *Medico::clone() const{ return new Medico(*this);}
 // Costruttore di Copia di Medico
-Medico::Medico(const Medico &m) : Dipendente(m), pazienti(m.pazienti) {}
+Medico::Medico(const Medico &m):Dipendente(m), pazienti(m.pazienti){}
 
-Medico* Medico::clone() const { return new Medico(*this); }
-
-double Medico::stipendio() const { return pagaPerOraMedico * numGiorniLavoro(); }
-
-vector<const Paziente*>& Medico::getPazienti() const { return pazienti; }
-
-void Medico::aggiungiPaziente(const Paziente* p) { pazienti.push_back(p); }
-
-void Medico::togliPaziente(const Paziente* p) {
-  vector<const Paziente*>::iterator it;
-  for(it = pazienti.begin(); it != pazienti.end(); ++it) {
-    if(*it == p) { pazienti.erase(it); }
-  }
+Medico &Medico::operator=(const Medico& m){
+    if(&m!=this){
+        this->Dipendente::operator=(m);
+        pazienti.clear();
+        pazienti=m.pazienti;
+    }
+    return *this;
 }
 
+double Medico::stipendio() const{ return pagaPerOraMedico*numGiorniLavoro()*8;}
+
+vector<Paziente *>& Medico::getPazienti(){ return pazienti;}
+
+vector<Paziente*>::iterator Medico::pazienteInLista(Paziente *p){
+    for(auto it=pazienti.begin(); it!=pazienti.end(); ++it){
+        if(*it==p){ return it;}
+    }
+    return pazienti.end();
+}
+
+void Medico::aggiungiPaziente(Paziente *p){
+    if(pazienteInLista(p)==pazienti.end()){
+        pazienti.push_back(p);
+        p->medicoAssegnato=this;
+    }
+    else{ std::cerr<<" Paziente già presente in lista";}
+}
+void Medico::cediPaziente(Paziente *p, Medico* m){
+    if(m!=this){
+        vector<Paziente*>::iterator it=pazienteInLista(p);
+        if(it!=pazienti.end()){
+            m->aggiungiPaziente(p);
+            pazienti.erase(it);
+        }
+    }
+    else{std::cerr<<"Il medico di destinazione è già assegnato al paziente"<<std::endl;}
+}
+
+///////////////////////////////////////////////////////////////////////////
+
 //INFERMIERE:
+double Infermiere::pagaPerOraInf=20.0;
+
 
 /* Costruttore di Infermiere
 * param id: Identificativo
@@ -105,51 +156,29 @@ void Medico::togliPaziente(const Paziente* p) {
 * param d:  Data di Nascita
 * param g:  Genere
 * param t:  Array booleano
-* param p:  Vettore di puntatori ad oggetti di tipo Paziente 
+* param p:  Vettore di puntatori ad oggetti di tipo Paziente
 * param r:  Valore Booleano, se true => responsabile
 */
-Infermiere::Infermiere(unsigned short id, string n, string c, const data& d, const gender& g, bool* t, bool r) :
-   Dipendente(id, n, c, d, g, t), responsabile(r) {}
+Infermiere::Infermiere(u_int id, string n, string c, const Data & d, const gender & g, const Turno& t, bool r):Dipendente(id,n,c,d,g,t),responsabile(r) {}
+Infermiere *Infermiere::clone() const{ return new Infermiere(*this);}
+
+double Infermiere::stipendio() const{ return pagaPerOraInf*numGiorniLavoro()*8;}
+bool Infermiere::isResponsabile() const{ return responsabile;}
+void Infermiere::cambiaResponsabile(bool x){responsabile=x;}
 
 // Costruttore di Copia di Infermiere
-Infermiere::Infermiere(const Infermiere& i) : Dipendente(i), responsabile(i.responsabile) {}
+Infermiere::Infermiere(const Infermiere& i):Dipendente(i), responsabile(i.responsabile){}
 
-Infermiere* Infermiere::clone() const { return new Infermiere(*this); }
+Infermiere &Infermiere::operator=(const Infermiere& i){
+    if(&i!=this){
+        this->Dipendente::operator=(i);
+        responsabile=i.responsabile;
+    }
+    return *this;
+}
 
-double Infermiere::stipendio() const { return pagaPerOraInf * numGiorniLavoro(); }
-bool Infermiere::isResponsabile() const { return responsabile; }
-void Infermiere::cambiaResponsabile(bool x) { responsabile = x; }
 
-//CHIRURGIA:
-
-/* Costruttore di Chirurgia
-* param idm: Identificativo Medico
-* param nm:  Nome Medico
-* param cm:  Cognome Medico
-* param dm:  Data di Nascita
-* param gm:  Genere del Medico
-* param tm:  Array booleano
-* param pm:  Vettore di puntatori ad oggetti di tipo Paziente 
-* param idi: Identificativo Infermiere
-* param ni:  Nome Infermiere
-* param ci:  Cognome Infermiere
-* param di:  Data di Nascita
-* param gi:  Genere
-* param ti:  Array booleano
-* param ri:  Valore Booleano, se true => responsabile
-* param paz:   Vettore di puntatori ad oggetti di tipo Paziente
-*/
-Chirurgia::Chirurgia(
-  // Medico
-  unsigned short idm, string nm, string cm, const data& dm, const gender& gm, bool* tm, vector<const Paziente*> pm,
-  // Infermiere
-  unsigned short idi, string ni, string ci, const data& di, const gender& gi, bool* ti, bool ri, 
-  // Paziente
-  vector<const Paziente*> paz) :
-  medChirurgia(new Medico(idm, nm, cm, dm, gm, tm, pm)), 
-  infChirurgia(new Infermiere(idi, ni, ci, di, gi, ti, ri)),
-  daOperare(paz) {}
-
+//////////////////////////////////////////////////////////////////////////
 //PAZIENTE:
 
 /* Costruttore di paziente
@@ -163,19 +192,104 @@ Chirurgia::Chirurgia(
 * param dec: Booleano
 * param m: Puntatore ad oggetto di tipo Medico
 */
-Paziente::Paziente(unsigned short id, string n, string c, const data& d, const gender& g, const data& ir, const data& fr, bool dec, Medico* m) :
-  Persona(id, n, c, d, g), inizioRicovero(ir), fineRicovero(fr), deceduto(dec), medicoAssegnato(m) {}
+Paziente::Paziente(unsigned short id,string n, string c, Medico* m, const Data & dn, const gender & g, const Data & ir, const Data & fr, bool dec): Persona(id,n,c,dn,g),inizioRicovero(ir),fineRicovero(fr), deceduto(dec), medicoAssegnato(m) {}
+Paziente::Paziente(const Paziente &p):Persona(p), inizioRicovero(p.inizioRicovero), fineRicovero(p.fineRicovero), deceduto(p.deceduto), medicoAssegnato(p.medicoAssegnato){}
+Paziente &Paziente::operator=(const Paziente& p){
+    if(&p!=this){
+        this->Persona::operator=(p);
+        inizioRicovero=p.inizioRicovero;
+        fineRicovero=p.fineRicovero;
+        deceduto=p.deceduto;
+        medicoAssegnato=p.medicoAssegnato;
+    }
+    return *this;
+}
 
-// Costruttore di Copia di Paziente
-Paziente::Paziente(const Paziente& p) : Persona(p), inizioRicovero(p.inizioRicovero), fineRicovero(p.fineRicovero), deceduto(p.deceduto), medicoAssegnato(p.medicoAssegnato) {};
+Paziente *Paziente::clone() const{ return new Paziente(*this);}
 
-Paziente* Paziente::clone() const { return new Paziente(*this); }
+Paziente::~Paziente(){};
 
-data Paziente::getInizioRic() const { return inizioRicovero; }
-data Paziente::getFineRic() const { return fineRicovero; }
-bool Paziente::isDeceduto() const { return deceduto; }
-Medico* Paziente::getMedicoAssegnato() const { return medicoAssegnato; }
+Data Paziente::getInizioRic()const{ return inizioRicovero;}
+Data Paziente::getFineRic() const {return fineRicovero;}
+bool Paziente::isDeceduto() const {return deceduto;}
+Medico* Paziente::getMedicoAssegnato() const {return medicoAssegnato;}
 
-void Paziente::modFineRicovero(const data& d) { fineRicovero = d; }
-void Paziente::modDeceduto(bool x){ if(x != deceduto) deceduto = x; }
-void Paziente::modMedicoAssegnato(Medico* m) { medicoAssegnato = m; }
+void Paziente::modFineRicovero(const Data &d){ fineRicovero=d;}
+void Paziente::modDeceduto(bool x){if(x!=deceduto)deceduto=x;}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+//OPERAZIONE:
+Operazione::Operazione(const Data& d, Medico* m, Infermiere* i):dataOp(d), medOp(m), infOp(i){}
+
+//metodi get
+Data Operazione::getDataOp() const{ return dataOp;}
+Medico* Operazione::getMedOp() const { return medOp;}
+Infermiere* Operazione::getInfOp() const {return infOp; }
+
+//modifica campi
+void Operazione::modDataOp(const Data& d){dataOp=d;}
+void Operazione::modMedOp(Medico* m){medOp=m;}
+void Operazione::modInfOp(Infermiere* i){infOp=i;}
+
+bool Operazione::operator==(const Operazione &o) const{ return dataOp==o.dataOp && medOp==o.medOp && infOp==o.infOp;}
+
+Operazione::Operazione(const Operazione& o){
+    dataOp=o.dataOp;
+    medOp=o.medOp;
+    infOp=o.infOp;
+}
+Operazione& Operazione::operator=(const Operazione& o){
+    if(this!=&o){
+        dataOp=o.dataOp;
+        medOp=o.medOp;
+        infOp=o.infOp;
+    }
+    return *this;
+}
+
+////////////////////////////////////////////////////////////////
+
+//PAZIENTE CHIRURGICO:
+
+
+/* Costruttore di PazienteChir
+* param idm: Identificativo Medico
+* param nm:  Nome Medico
+* param cm:  Cognome Medico
+* param dm:  Data di Nascita
+* param gm:  Genere del Medico
+* param tm:  Array booleano
+* param pm:  Vettore di puntatori ad oggetti di tipo Paziente
+* param idi: Identificativo Infermiere
+* param ni:  Nome Infermiere
+* param ci:  Cognome Infermiere
+* param di:  Data di Nascita
+* param gi:  Genere
+* param ti:  Array booleano
+* param ri:  Valore Booleano, se true => responsabile
+* param paz:   Vettore di puntatori ad oggetti di tipo Paziente
+*/
+PazienteChir::PazienteChir(unsigned short id, string name, string sur, Medico* med, const Data& dOp, Medico* mOp, Infermiere* iOp, Persona* par, const Data& nasc, const gender& g, const Data& iRic, const Data& fRic, bool dec, bool succ):Paziente(id, name, sur, med, nasc, g, iRic, fRic, dec), Operazione(dOp,mOp,iOp), conSuccesso(succ), parente(*par){}
+
+//metodi get
+bool PazienteChir::getConSuccesso() const { return conSuccesso;}
+Persona& PazienteChir::getParente() const { return parente;}
+
+PazienteChir *PazienteChir::clone() const{ return new PazienteChir(*this);}
+
+//metodi modifica
+void PazienteChir::modConSuccesso(bool x){ conSuccesso=x;}
+void PazienteChir::modParente(const Persona& p){ parente=p;}
+
+PazienteChir::PazienteChir(const PazienteChir& o):Paziente(o),Operazione(o),conSuccesso(o.conSuccesso), parente(o.parente){}
+PazienteChir& PazienteChir::operator=(const PazienteChir& o){
+    if(&o!=this){
+        this->Paziente::operator=(o);
+        this->Operazione::operator=(o);
+        conSuccesso=o.conSuccesso;
+        parente.Persona::operator=(o.parente);
+    }
+    return *this;
+}
