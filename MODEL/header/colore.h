4210d9a377c426fa36afb84e87227fec1abd0d8f
marcoody <57504229+marcoody@users.noreply.github.com>
2020-07-29 14:59:13 +0200

    Create colore.h

---
+++ MODEL/header/colore.h
@@ -0,0 +1,13 @@
+#ifndef COLORE_H
+#define COLORE_H
+
+
+class Colore{
+private:
+    unsigned short int r,g,b;
+
+public:
+    Colore(unsigned short int, unsigned short int, unsigned short int);
+};
+
+#endif // COLORE_H
