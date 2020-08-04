fdee99721edee041855497a72cc12857e4023e1d
marcoody <57504229+marcoody@users.noreply.github.com>
2020-08-04 14:46:50 +0200

    Update turno.cpp

--- MODEL/source/turno.cpp
+++ MODEL/source/turno.cpp
@@ -1,13 +1,13 @@
 #include "../header/turno.h"
 
-Turno::Turno(Data& d, u_int o): dataTurno(d), nOre(o){}
+Turno::Turno(giorni g, u_int o): giornoTurno(g), nOre(o){}
 
 //metodi get
-Data Turno::getDataTurno() const { return dataTurno; }
+giorni Turno::getGiornoTurno() const { return giornoTurno; }
 u_int Turno::getNOre() const { return nOre; }
 
 //metodi set
-void Turno::setDataTurno( Data& d) { dataTurno=d; }
+void Turno::setGiornoTurno(giorni& g) { giornoTurno=g; }
 void Turno::setNOre(u_int o) { nOre=o; }
 
 //metodi export
@@ -17,19 +17,29 @@ void Turno::exportXml(QXmlStreamWriter& out) const{
     out.writeEndElement();
 }
 void Turno::exportXmlData(QXmlStreamWriter& out) const{
-    out.writeTextElement("dataTurno", QString::fromStdString(getDataTurno().dataToString()));
+    string g = giornoToString(getGiornoTurno());
+    out.writeTextElement("giornoTurno", QString::fromStdString(g));
     out.writeTextElement("nOre", QString::number(getNOre()));
 }
 
 //metodi import
-void Turno::importXmlData(QXmlStreamReader& in, Data& dataTurno, u_int& nOre){
+void Turno::importXmlData(QXmlStreamReader& in, giorni& giornoTurno, u_int& nOre){
     string oreStringa;
-    string dataStringa;
-    importTagXml(in, "dataTurno", dataStringa);
+    string giornoStringa;
+    importTagXml(in, "giornoTurno", giornoStringa);
     importTagXml(in, "nOre", oreStringa);
-    dataTurno= stringToData(dataStringa);
+    giornoTurno= stringToGiorni(giornoStringa);
     nOre= stoull(oreStringa);
 }
+
+
+string Turno::infoTurno() const{
+    return "Tipo Turno: " + getTag() + "\nGiorno: " + (giornoToString(getGiornoTurno())) + "\nNumero di Ore: " + std::to_string(getNOre());
+}
+
+string Turno::getTag() const { return "TURNO"; }
+
+
 
 
 string Turno::infoTurno() const{
