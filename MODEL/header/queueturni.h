7fb966f7ce39509a2b79594fed1ce3033206f47e
marcoody <57504229+marcoody@users.noreply.github.com>
2020-08-04 14:38:07 +0200

    Update queueturni.h

--- MODEL/header/queueturni.h
+++ MODEL/header/queueturni.h
@@ -1,26 +1,38 @@
-#ifndef RESPONSABILE_H
-#define RESPONSABILE_H
-#include <string>
-#include "persona.h"
-using std::string;
+#ifndef QUEUETURNI_H
+#define QUEUETURNI_H
+#include "turno.h"
+#include "turno_regolare.h"
+#include "turno_intero.h"
+#include "turno_parziale.h"
+#include "turno_libero.h"
+#include "turno_straordinario.h"
+#include "queue.h"
 
-class Responsabile: public Persona{
+
+class QueueTurni: public Queue<Turno*>{
+private:
+    static QString startTag;
+    static QString defaultFile;
+    QString getStartTagXml() const;
+    QString getDefaultFile() const;
 public:
-    Responsabile(string, string, string = "", string ="", const Data& = Data(), const gender& = gender::altro);
+    QueueTurni(): Queue(){
+        giorni g = giorni::lunedi;
+        for(int i=0; i<6; ++i){
+            Turno_intero* tmp = new Turno_intero(g, 8, "");
+            push_back(tmp->clone());
+            delete tmp;
+            avanzaGiorni(g);
+        }
+        Turno_libero* tmp = new Turno_libero(giorni::domenica);
+        push_back(tmp->clone());
+        delete tmp;
+    };
 
-    //permessi
-    virtual bool isResponsabile() const;
-    virtual bool canAddTurni() const;
-    virtual bool canEditTurni() const;
-    virtual Responsabile* clone() const;
-    virtual string getTag() const;
-    virtual double stipendio() const;
-    //infoPersona ereditato da Persona
 
-    //import export
-    //export ereditato da Persona
-    static Persona* importXml(QXmlStreamReader&);
+    bool importXml(QXmlStreamReader&);
+    double stipendioTurni() const;
+
 };
 
-#endif // RESPONSABILE_H
-
+#endif // QUEUETURNI_H
