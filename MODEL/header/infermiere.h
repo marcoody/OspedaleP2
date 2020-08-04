4df21c101f483cc9b3fde8b898775b86f4cd16e8
marcoody <57504229+marcoody@users.noreply.github.com>
2020-08-04 14:32:08 +0200

    Update infermiere.h

--- MODEL/header/infermiere.h
+++ MODEL/header/infermiere.h
@@ -8,17 +8,25 @@ using std::string;
 class Infermiere: public Persona{
 private:
     QueueTurni turni;
-    virtual string getTag() const;
+
 public:
     Infermiere(string, string, string = "", string ="", const Data& = Data(), const gender& = gender::altro, const QueueTurni& = QueueTurni());
 
     //metodi get
     QueueTurni getTurni() const;
+
+    //metodi set
+    void setTurni(giorni, Turno*);
     //permessi
     virtual bool isResponsabile() const;
     virtual bool canAddTurni() const;
     virtual bool canEditTurni() const;
+
+    //virtual
     virtual Infermiere* clone() const;
+    virtual string getTag() const;
+    virtual double stipendio() const;
+    //infoPersona ereditato da Persona
 
     virtual void exportXml(QXmlStreamWriter &) const;
     static void importXmlData(QXmlStreamReader&, QueueTurni&);
