27e84ac953160f21e94639ab4fc17ba63c2394a8
marcoody <57504229+marcoody@users.noreply.github.com>
2020-08-06 18:20:45 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,20 @@
 
-#include <iostream>
-#include "MODEL/header/queue.h"
-
-using std::cout;
-using std::endl;
-
-int main(){
+#include <QMessageBox>
 
 
+int main(int argc, char **argv)
+{
+    QApplication app(argc, argv);
 
+    QueuePersone utenti;
+    Responsabile* r = new Responsabile("ciao", "ciao", "ciao","ciao");
 
+    utenti.push_back(r->clone());
 
+    bool relogin = false;
+    Main_dialog* mw = new Main_dialog(utenti,r, relogin);
+    mw->show();
+    app.exec();
 
-
+    return 0;
 }
