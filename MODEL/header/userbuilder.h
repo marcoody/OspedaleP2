28fcfe0628c4995d111fbd6dbf752e92ea3aa1e9
marcoody <57504229+marcoody@users.noreply.github.com>
2020-08-11 16:44:23 +0200

    Update userbuilder.h

--- MODEL/header/userbuilder.h
+++ MODEL/header/userbuilder.h
@@ -1,18 +1,26 @@
 #ifndef USERBUILDER_H
 #define USERBUILDER_H
 #include "../../MODEL/header/persona.h"
+#include "../../MODEL/header/turno.h"
 #include <string>
 #include <QString>
 #include <QXmlStreamReader>
+
 #include "../../MODEL/header/medico.h"
 #include "../../MODEL/header/infermiere.h"
 #include "../../MODEL/header/responsabile.h"
+
+#include "../../MODEL/header/turno_intero.h"
+#include "../../MODEL/header/turno_libero.h"
+#include "../../MODEL/header/turno_parziale.h"
+#include "../../MODEL/header/turno_straordinario.h"
 
 
 
 class UserBuilder{
 public:
     static Persona* build(const QString&);
+    static Turno* buildT(const QString&, Turno *prec);
     static Persona* buildWithXmlTag(const std::string&, QXmlStreamReader&);
     virtual ~UserBuilder() = 0;
 };
